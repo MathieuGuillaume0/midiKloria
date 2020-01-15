a80ce616aead6ea51082568b40cf486463a8eb64
Mathieu Guillaume <mguillaume00@gmail.com>
2020-01-15 21:49:02 +0100

    read values and set LEDs outputs

---
+++ poc_midikloria.ino
@@ -0,0 +1,61 @@
+#define POTAR_PIN A0
+#define SLIDER_PIN A1
+#define BUTTON0_PIN 10
+#define BUTTON1_PIN 9
+#define BUTTON2_PIN 8
+#define LED0_PIN 13
+#define LED1_PIN 12
+#define LED2_PIN 11
+#define MAX_ANALOG_VALUE 714.0f  // choosen by experiments with sensors
+
+int potarValue;
+int sliderValue;
+float potarRatio;
+
+bool button0Value;
+bool button1Value;
+bool button2Value;
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+
+  // configure button pins as as pull up input
+  pinMode(BUTTON0_PIN, INPUT_PULLUP);
+  pinMode(BUTTON1_PIN, INPUT_PULLUP);
+  pinMode(BUTTON2_PIN, INPUT_PULLUP);
+
+  // configure leds pin as output
+  pinMode(LED0_PIN, OUTPUT);
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+
+}
+
+void loop() {
+  // get analog values
+  potarValue = analogRead(POTAR_PIN);
+  sliderValue = analogRead(SLIDER_PIN);
+  potarRatio = potarValue / MAX_ANALOG_VALUE;
+  
+  // set pwm duty cycle of led 2 according to potar ratio
+  analogWrite(LED2_PIN, (byte) (potarRatio*255));
+  
+  // get digital values buttons
+  button0Value = digitalRead(BUTTON0_PIN);
+  button1Value = digitalRead(BUTTON1_PIN);
+  button2Value = digitalRead(BUTTON2_PIN);
+
+  // toggle led 0 every loop
+  digitalWrite(LED0_PIN, !digitalRead(LED0_PIN));
+  
+  // print values
+//  Serial.println(potarValue / MAX_ANALOG_VALUE);
+//  Serial.println(potarRatio*255);
+
+  
+  // wait
+  delay(20);  
+   
+
+}
