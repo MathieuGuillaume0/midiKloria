89661b58248e57c060db56f4598778f0c366b9e1
Mathieu Guillaume <mguillaume00@gmail.com>
2020-01-16 18:12:11 +0100

    filter analog values

--- poc_midikloria.ino
+++ poc_midikloria.ino
@@ -1,5 +1,6 @@
 #include <MIDI.h>
 
+
 // symbolic pin names
 #define POTAR_PIN A0
 #define SLIDER_PIN A1
@@ -13,11 +14,27 @@
 // project related defines
 #define MAX_ANALOG_VALUE 714.0f  // choosen by experiments with sensors
 #define DEFAULT_CHANNEL 1
+#define AVG_SAMPLES_NUM 3
 
+// project variables
 int potarValue, sliderValue;
-int lastpotarValue = -1, lastsliderValue = -1;
-float potarRatio, sliderRatio;
+int potarSum = 0, sliderSum = 0;
+int potarAvg, lastpotarAvg, sliderAvg, lastsliderAvg;
+int avgPos = 0;
+int potarArray[AVG_SAMPLES_NUM] = {0}, sliderArray[AVG_SAMPLES_NUM] = {0};
 bool button0Value, button1Value, button2Value;
+
+// functions
+int movingAvg(int *numArray, int arraySize, int *sum, int nextNum, int pos){
+  // Subtract the oldest number from the prev sum, add the new number
+  *sum = *sum - numArray[pos] + nextNum;
+  
+  // Assign the nextNum to the position in the array
+  numArray[pos] = nextNum;
+
+  // return the average
+  return *sum / arraySize;
+}
 
 // Create and binds the MIDI interface to the default hardware Serial port
 MIDI_CREATE_DEFAULT_INSTANCE();
@@ -43,21 +60,27 @@ void loop() {
   // get analog values
   potarValue = analogRead(POTAR_PIN);
   sliderValue = analogRead(SLIDER_PIN);
-  potarRatio = potarValue / MAX_ANALOG_VALUE;
-  sliderRatio = sliderValue / MAX_ANALOG_VALUE;
+
+  // moving average off these values
+  potarAvg = movingAvg(potarArray, AVG_SAMPLES_NUM, &potarSum, potarValue, avgPos);
+  sliderAvg = movingAvg(sliderArray, AVG_SAMPLES_NUM, &sliderSum, sliderValue, avgPos);
+
+  // increase moving average positon for the next loop
+  avgPos++;
+  if (avgPos >= AVG_SAMPLES_NUM) avgPos = 0;
   
   // send midi control change if one of the analog value changed
-  if (potarValue != lastpotarValue){
-    lastpotarValue = potarValue;
-    MIDI.sendControlChange(1, (byte)(potarRatio*127), DEFAULT_CHANNEL);
+  if (potarAvg != lastpotarAvg){
+    lastpotarAvg = potarAvg;
+    MIDI.sendControlChange(1, (byte)((potarAvg / MAX_ANALOG_VALUE)*127), DEFAULT_CHANNEL);
   }
-  if (sliderValue != lastsliderValue){
-    lastsliderValue = sliderValue;
-    MIDI.sendControlChange(2, (byte)(sliderValue*127), DEFAULT_CHANNEL);
+  if (sliderAvg != lastsliderAvg){
+    lastsliderAvg = sliderAvg;
+    MIDI.sendControlChange(2, (byte)((sliderAvg / MAX_ANALOG_VALUE)*127), DEFAULT_CHANNEL);
   }
   
   // set pwm duty cycle of led 2 according to potar ratio
-  analogWrite(LED2_PIN, (byte) (potarRatio*255));
+  analogWrite(LED2_PIN, (byte) ((potarValue / MAX_ANALOG_VALUE)*255)); 
   
   // get digital values buttons
   button0Value = digitalRead(BUTTON0_PIN);
@@ -68,9 +91,9 @@ void loop() {
   digitalWrite(LED0_PIN, !digitalRead(LED0_PIN));
   
   // print values
-//  Serial.println(potarValue / MAX_ANALOG_VALUE);
+//  Serial.println(potarAvg);
 //  Serial.println(potarRatio*255);
-
+  
   
   // wait
   delay(50);  
