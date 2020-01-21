0274fcb18a6fef337b4e8b7101b7cba7bb28b9bd
Mathieu Guillaume <mguillaume00@gmail.com>
2020-01-21 15:58:17 +0100

    add mux id in potar struct and define mux pins array

--- poc_midikloria.ino
+++ poc_midikloria.ino
@@ -5,26 +5,30 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 /** project related defines **/
 #define BUTTONS_NUM 3
-#define POTARS_NUM 2
 #define LEDS_NUM 3
+#define DIRECT_POTARS_NUM 2  // number of potars directly wired to an arduino pin
+#define MUX_POTARS_NUM 2  // number of potars wired to the input multiplexer
+#define POTARS_NUM (DIRECT_POTARS_NUM + MUX_POTARS_NUM) // total number of potars
 
 #define DEFAULT_CHANNEL 1
 #define DEFAULT_VELOCITY 127
-
 #define POTARS_CC_OFFSET 1  // control change start id for buttons
 #define BUTTONS_CC_OFFSET (POTARS_CC_OFFSET + POTARS_NUM)  // control change start id for buttons
 #define NOTE_OFFSET 50  // note start id for buttons
+#define BUTTON_CC_MODE  // buttons will send CC msgs. comment this if you want note msgs
 
 #define MAX_ANALOG_VALUE 714.0f  // choosen by experiments with sensors
 #define AVG_SAMPLES_NUM 3  // number of samples used in the moving average
 #define ANALOG_DIFF_THRES 1  // difference threshold between last and current value until a MIDI msg is sent
-//#define BUTTON_NOTE_MODE  // buttons will send notes msgs. comment this if you want CC msgs. 
+
 
 
 /** pins **/
-byte potarsPins[POTARS_NUM] = {A0, A1}; 
+byte potarsPins[POTARS_NUM] = {A1, A2, A0, A0}; 
+int potarsMuxIds[POTARS_NUM] = {-1, -1, 0, 1};  // -1 means theres no multiplexer between the potar and the board pin
 byte buttonsPins[BUTTONS_NUM] = {7, 8, 9};  
-byte ledsPins[LEDS_NUM] = {13, 10, 11};  
+byte ledsPins[LEDS_NUM] = {13, 10, 11}; 
+byte muxPins[] = {0, 1, 2};
 
 /** pot variables **/
 byte avgPos = 0;
@@ -32,9 +36,9 @@ struct potar {
 	byte pinNumber;
 	int lastValue, sum = 0;
 	int previousValues[AVG_SAMPLES_NUM] = {0};
+	int muxId;
 };
 struct potar potars[POTARS_NUM];
-
 
 /** button variables**/
 struct button {
@@ -60,15 +64,15 @@ void updateButton(struct button *buttonsArray, byte id){
 	struct button *thisButton = buttonsArray + id;
 	bool buttonState = digitalRead(thisButton->pinNumber);
 	
-	#ifdef BUTTON_NOTE_MODE  // send note msg
-	if (buttonState != thisButton->state){
-		if (!buttonState) MIDI.sendNoteOn(NOTE_OFFSET + id, DEFAULT_VELOCITY, DEFAULT_CHANNEL);  // note on when button is pressed
-		else MIDI.sendNoteOff(NOTE_OFFSET + id, DEFAULT_VELOCITY, DEFAULT_CHANNEL);  // note off when button is released
-	}
-	#else  // send CC msg 
+	#ifdef BUTTON_CC_MODE  // send cc msg
 	if ((buttonState != thisButton->state) && !buttonState){  // only when button is pressed
 		thisButton->value = !thisButton->value;  
 		MIDI.sendControlChange(BUTTONS_CC_OFFSET + id, thisButton->value*127, DEFAULT_CHANNEL);  // send enable or disable request regarding the previous value 
+	}
+	#else  // send note msg
+	if (buttonState != thisButton->state){
+		if (!buttonState) MIDI.sendNoteOn(NOTE_OFFSET + id, DEFAULT_VELOCITY, DEFAULT_CHANNEL);  // note on when button is pressed
+		else MIDI.sendNoteOff(NOTE_OFFSET + id, DEFAULT_VELOCITY, DEFAULT_CHANNEL);  // note off when button is released
 	}
 	#endif
 	thisButton->state = buttonState;  // save current button state
@@ -78,6 +82,13 @@ void updatePotar(struct potar *potarsArray, byte id, byte pos){
 	struct potar *thisPotar = potarsArray + id;
 	
 	// get analog value
+	if (thisPotar->muxId){  // drive multiplexer pins before read if this a mux input 
+		byte muxId = (byte)thisPotar->muxId;
+		for(int i=0; i < 3; i++){
+			digitalWrite(muxPins, (muxId >> i) & 0x01);
+		}
+		delay(5);  // wait a bit before reading
+	}
 	int potarValue = (analogRead(thisPotar->pinNumber) / MAX_ANALOG_VALUE)*127;
 
 	// moving average 
@@ -98,9 +109,10 @@ void setup() {
 	MIDI.begin(MIDI_CHANNEL_OMNI);
 	Serial.begin(115200);
 	
-	// initialize potars structure
+	// initialize potars structure with pins and mux ids
 	for(int i=0; i < POTARS_NUM; i++){
 		potars[i].pinNumber = potarsPins[i];
+		potars[i].muxId = potarsMuxIds[i];
 	}
 	
 	// configure button pins as pull up input and init structure
@@ -109,9 +121,12 @@ void setup() {
 		buttons[i].pinNumber = buttonsPins[i];
 	}
 	
-	// configure leds pins as output
+	// configure leds and mux pins as output
 	for(int i=0; i < LEDS_NUM; i++){
 		pinMode(ledsPins[i], OUTPUT);
+	}
+	for(int i=0; i < 3; i++){
+		pinMode(muxPins[i], OUTPUT);
 	}
 
 }
